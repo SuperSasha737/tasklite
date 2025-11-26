802a22817fd09700a9487393954ba8c7e0011d57
Alex737 <covalewjenj@yandex.ru>
2025-11-26 18:36:23 +0300

    update: file package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+const repoName = "tasklite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: `/${repoName}`
 })
